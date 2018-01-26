4c2e641530a06da5a542897328b47bb9c37fd1ee
T0asted <josh@etherealgames.co.uk>
2018-01-26 22:10:20 +0000

    Josh First Update
    started Projectile movement

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    Rigidbody2D RB;
+
+    float Angle;
+
+	// Use this for initialization
+	void Start () {
+        RB = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        SetRotation();
+
+    }
+
+    void SetRotation()
+    {
+        //find Angel
+
+        //Angle = ;
+
+        //Set Rotation
+
+        RB.rotation = Angle;
+    }
+}
