27d109898d2ae0eccc177fb8a7b1d596a09ad6b2
Dean Sinclair <deano.sinclair@yahoo.co.uk>
2018-01-27 15:28:33 +0000

    Added player attacking to the player controller.

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -10,12 +10,20 @@ public class PlayerController : MonoBehaviour {
     public float inAirDamping = 5f;
     public float jumpHeight = 3f;
 
+    public GameObject projectilePrefab;
+
     [HideInInspector]
     private float normalizedHorizontalSpeed = 0;
 
     private CharacterController2D _controller;
     private Animator _animator;
     private Vector3 _velocity;
+
+    private int _playerID = 1;
+
+    private bool _isChargingAttack = false;
+    private float _chargingProgress;
+    private float _chargingSpeed = 3;
 
     #region Event Listeners
 
@@ -50,6 +58,53 @@ public class PlayerController : MonoBehaviour {
         _controller.OnTriggerExitEvent += onTriggerExitEvent;
     }
 
+    public void Initialise(int id) {
+        _playerID = id;
+    }
+
+    private void Update() {
+        ProcessAttack();
+    }
+
+    private void ProcessAttack() {
+        float attackInput = Input.GetAxis(GetControlString(PlayerControls.Attack));
+        if (attackInput > 0) {
+            // Charge attack
+            Debug.Log("Charging: " + _chargingProgress);
+            _chargingProgress = Mathf.Clamp01(_chargingProgress + Time.deltaTime / _chargingSpeed);
+            _isChargingAttack = true;
+        } else if (_isChargingAttack) {
+            ReleaseAttack();
+        }
+    }
+
+    private void ReleaseAttack() {
+        // Release an attack based on how long the player has charged their attack.
+        if (_chargingProgress < 0.25f) {
+            // do nothing, not charged enough
+            Debug.Log("Attack failed");
+        } else {
+            GameObject projectileGO = Instantiate(projectilePrefab) as GameObject;
+            projectileGO.transform.position = transform.position;
+            Projectile projectile = projectileGO.GetComponent<Projectile>();
+            projectile.Initialise(_playerID, GetAttackDirection(), _chargingProgress * 10);
+            Debug.Log("Attack released");
+        }
+        _chargingProgress = 0;
+        _isChargingAttack = false;
+    }
+
+    private Vector3 GetAttackDirection() {
+        var horizontal = Input.GetAxis(GetControlString(PlayerControls.HorizontalAim));
+        var vertical = Input.GetAxis(GetControlString(PlayerControls.VerticalAim));
+
+        if (horizontal == 0 && vertical == 0) {
+            horizontal = 1;
+        }
+
+        return new Vector2(horizontal, vertical).normalized;
+    }
+
     private void FixedUpdate() {
         UpdatePhysics();
     }
@@ -59,7 +114,7 @@ public class PlayerController : MonoBehaviour {
             _velocity.y = 0;
         }
 
-        if (Input.GetAxis("Horizontal") > 0) {
+        if (Input.GetAxis(GetControlString(PlayerControls.Movement)) > 0) {
             normalizedHorizontalSpeed = 1;
             if (transform.localScale.x < 0f) {
                 transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
@@ -68,7 +123,7 @@ public class PlayerController : MonoBehaviour {
             if (_controller.IsGrounded) {
                 //_animator.Play(Animator.StringToHash("Run"));
             }
-        } else if (Input.GetAxis("Horizontal") < 0) {
+        } else if (Input.GetAxis(GetControlString(PlayerControls.Movement)) < 0) {
             normalizedHorizontalSpeed = -1;
             if (transform.localScale.x > 0f) {
                 transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
@@ -85,7 +140,7 @@ public class PlayerController : MonoBehaviour {
         }
 
         // we can only jump whilst grounded
-        if (_controller.IsGrounded && Input.GetButtonDown("Jump")) {
+        if (_controller.IsGrounded && Input.GetButtonDown(GetControlString(PlayerControls.Jump))) {
             _velocity.y = Mathf.Sqrt(2f * jumpHeight * -GameManager.GRAVITY);
             //_animator.Play(Animator.StringToHash("Jump"));
         }
@@ -108,5 +163,29 @@ public class PlayerController : MonoBehaviour {
 
         // grab our current _velocity to use as a base for all calculations
         _velocity = _controller.velocity;
+    }
+
+    private string GetControlString(PlayerControls control) {
+        switch (control) {
+            case PlayerControls.Movement:
+                return string.Format("Player{0}Movement", _playerID);
+            case PlayerControls.HorizontalAim:
+                return string.Format("Player{0}AimHorizontal", _playerID);
+            case PlayerControls.VerticalAim:
+                return string.Format("Player{0}AimVertical", _playerID);
+            case PlayerControls.Jump:
+                return string.Format("Player{0}Jump", _playerID);
+            case PlayerControls.Attack:
+                return string.Format("Player{0}Attack", _playerID);
+        }
+        return "";
+    }
+
+    public enum PlayerControls {
+        Movement,
+        HorizontalAim,
+        VerticalAim,
+        Jump,
+        Attack
     }
 }
