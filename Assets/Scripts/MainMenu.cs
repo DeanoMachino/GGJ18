32b1fa088227b201ec1f8a1f7043422ad15cda41
GeekMasher <geekmasher@gmail.com>
2018-01-27 11:50:37 +0000

    Init MainMenu commit
    - Added Font
    - Basic Script for quiting the game
    - Basic layout for the menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor.UI;
+
+public class MainMenu : MonoBehaviour {
+
+
+    public void Start()
+    {
+        // TODO: Randomly pick a dog to display
+    }
+
+	// Quits game when called
+    public void onQuitEvent()
+    {
+        Debug.Log("Exiting Game");
+        Application.Quit();
+    }
+}
